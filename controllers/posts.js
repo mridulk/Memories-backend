b656e220334f974fefd91a271f60364a1b661d32
mridulk <mridulkh0302@gmail.com>
2021-03-08 22:02:42 +0530

    like functionality updated

--- controllers/posts.js
+++ controllers/posts.js
@@ -35,3 +35,10 @@ export const deletePost=async(req,res)=>{
     res.json({message:'Successfully Deleted the memory'})
 
 }
+export const likePost=async(req,res)=>{
+    const {id}=req.params;
+    if(!mongoose.Types.ObjectId.isValid(id))return res.status(404).send('No post with that id')
+    const post= await PostMessage.findById(id);
+    const updatePost=await PostMessage.findByIdAndUpdate(id,{likeCount:post.likeCount+1},{new:true});
+    res.json(updatePost);
+}
--- routes/posts.js
+++ routes/posts.js
@@ -1,8 +1,10 @@
 import express from 'express'
-import {updatePost,getPosts,createPost,deletePost} from '../controllers/posts.js'
+import {updatePost,getPosts,createPost,deletePost,likePost} from '../controllers/posts.js'
 const router=express.Router();
  router.get('/',getPosts)
  router.post('/',createPost);
  router.patch('/:id',updatePost);
  router.delete('/:id',deletePost);
+ router.patch('/:id/likePost',likePost);
+ 
  export default router;
