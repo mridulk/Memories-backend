a55e2f15fd0cb0da8f1a493e812fde104c46a407
mridulk <mridulkh0302@gmail.com>
2021-03-10 07:29:41 +0530

    set env variables

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv'
 import postRoutes from './routes/posts.js';
 
 const app = express();
-
+dotenv.config();
 //every route inside of postRoute will start from /posts
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
@@ -13,13 +14,15 @@ app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
 
 app.use('/posts', postRoutes);
-
+app.get('/',(req,res)=>{
+  res.send('Hello to Memories API')
+})
 //Database Connection
-const CONNECTION_URL =
-  'mongodb+srv://admin:1234@cluster0.g118i.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+// const CONNECTION_URL =
+//   'mongodb+srv://admin:1234@cluster0.g118i.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 mongoose
-  .connect(CONNECTION_URL, {
+  .connect(process.env.CONNECTION_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
